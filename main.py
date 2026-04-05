6108e4519c891046ad0a9aa79c67ea3bc0833ce4
vignesh <vigneshkomma@gmail.com>
2026-04-05 18:22:57 +0530

    first commit: version 1.0

---
+++ main.py
@@ -0,0 +1,30 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+items = []
+
+@app.get("/")
+async def root():
+    return{"message:":"TO DO LIST API:v1.0"}
+
+
+@app.post("/items/insert")
+def insert(item:str):
+    items.append(item)
+    return item
+
+@app.get("/items/view")
+def view():
+    return items
+
+@app.delete("/items/remove")
+def remove(id:int) -> str:
+    items.remove(items[id])
+    return "Task removed" 
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",host="127.0.0.1",port=8000,reload=True)
