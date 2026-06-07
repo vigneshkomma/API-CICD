4178f7ae1399d398aaadf88b552dcba7e6be299b
vignesh <vigneshkomma@gmail.com>
2026-06-07 15:13:19 +0530

    Test Github Actions 2

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 @app.get("/")
 def root():
     return {
-        "message": "URL Health Checker API is running",
+        "message": "URL Health Checker API is running: version:1.0.1",
         "docs": "/docs"
     }
 
